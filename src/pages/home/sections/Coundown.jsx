5976c4494e900b5b726308fb97abbd9ed3adb810
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-09-01 20:28:59 +0600

    coundown added

---
+++ src/pages/home/sections/Coundown.jsx
@@ -0,0 +1,12 @@
+
+const Coundown = ({count}) => {
+    return (
+        <div
+            className={`fixed z-10 top-0 left-0 w-full h-full bg-[#00000085] transition text-8xl text-white grid items-center`}
+        >
+            {count}
+        </div>
+    );
+};
+
+export default Coundown;
